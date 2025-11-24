7a0377655018a18dca192619cb7d291ad394c38f
damar <damarhati123@gmail.com>
2025-11-25 02:57:19 +0700

    malammalamkugabut

--- toko_komputer.sql
+++ toko_komputer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Nov 2025 pada 09.50
+-- Waktu pembuatan: 24 Nov 2025 pada 20.56
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -40,7 +40,15 @@ CREATE TABLE `aktifitas` (
 --
 
 INSERT INTO `aktifitas` (`id_aktifitas`, `id_karyawan`, `jenis_aktifitas`, `keterangan`, `tanggal`) VALUES
-(1, '', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 1.685.000', '2025-11-16 09:45:54');
+(1, '', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 1.685.000', '2025-11-16 09:45:54'),
+(2, 'KRY001', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 1.685.000', '2025-11-24 03:28:48'),
+(3, 'KRY001', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 2.002.000', '2025-11-24 03:33:01'),
+(4, 'KRY001', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 1.300.000', '2025-11-24 03:34:07'),
+(5, 'KRY001', 'barang_masuk', 'Menerima barang masuk: ACER PREDATOR VESTA RGB SILVER DDR4 16GB (2X8GB) sebanyak 4 unit', '2025-11-24 09:42:17'),
+(6, 'KRY001', 'barang_keluar', 'Mengeluarkan barang: ACER PREDATOR VESTA RGB SILVER DDR4 16GB (2X8GB) sebanyak 1 unit - pindah gudang', '2025-11-24 09:42:43'),
+(7, 'KRY001', 'transaksi', 'Melakukan transaksi penjualan dengan total Rp 2.002.000', '2025-11-24 03:55:12'),
+(8, 'KRY001', 'barang_masuk', 'Menerima barang masuk: ACER PREDATOR VESTA RGB SILVER DDR4 16GB (2X8GB) sebanyak 6 unit', '2025-11-25 01:43:07'),
+(9, 'KRY001', 'barang_keluar', 'Mengeluarkan barang: DeepCool FL12R 3-IN-1 12CM Unique Addressable RGB Fan sebanyak 3 unit - display toko', '2025-11-25 01:47:37');
 
 -- --------------------------------------------------------
 
@@ -67,7 +75,16 @@ INSERT INTO `barang_keluar` (`id_keluar`, `id_produk`, `jumlah_keluar`, `tanggal
 ('BK004', 'PSU002', 1, '2025-11-16', ''),
 ('BK005', 'RAM005', 1, '2025-11-16', ''),
 ('BK006', 'PSU002', 1, '2025-11-16', ''),
-('BK007', 'RAM005', 1, '2025-11-16', '');
+('BK007', 'RAM005', 1, '2025-11-16', ''),
+('BK008', 'PSU002', 1, '2025-11-24', 'KRY001'),
+('BK009', 'RAM005', 1, '2025-11-24', 'KRY001'),
+('BK010', 'RAM002', 1, '2025-11-24', 'KRY001'),
+('BK011', 'FAN009', 1, '2025-11-24', 'KRY001'),
+('BK012', 'RAM002', 1, '2025-11-24', 'KRY001'),
+('BK013', 'RAM005', 1, '2025-11-24', 'KRY001'),
+('BK014', 'RAM002', 1, '2025-11-24', 'KRY001'),
+('BK015', 'FAN009', 1, '2025-11-24', 'KRY001'),
+('BK016', 'FAN005', 3, '2025-11-24', 'KRY001');
 
 -- --------------------------------------------------------
 
@@ -84,6 +101,14 @@ CREATE TABLE `barang_masuk` (
   `id_karyawan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_masuk`, `id_produk`, `id_supplier`, `jumlah_masuk`, `tanggal`, `id_karyawan`) VALUES
+('BM001', 'RAM005', 'SUP003', 4, '2025-11-24', 'KRY001'),
+('BM002', 'RAM005', 'SUP002', 6, '2025-11-24', 'KRY001');
+
 -- --------------------------------------------------------
 
 --
@@ -93,8 +118,17 @@ CREATE TABLE `barang_masuk` (
 CREATE TABLE `customer` (
   `id_customer` varchar(20) NOT NULL,
   `nama` text NOT NULL,
-  `email` text NOT NULL
+  `nomor_telepon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `customer`
+--
+
+INSERT INTO `customer` (`id_customer`, `nama`, `nomor_telepon`) VALUES
+('CUS001', 'Deni Setiawan', 0),
+('CUS002', 'Yogi Aprilianto', 0),
+('CUS003', 'Achmad Nazzri', 0);
 
 -- --------------------------------------------------------
 
@@ -118,7 +152,14 @@ INSERT INTO `detail_transaksi` (`id_detail`, `id_transaksi`, `id_produk`, `jumla
 ('DTL001', 'TRX001', 'PSU002', 1, 785000),
 ('DTL002', 'TRX001', 'RAM005', 1, 900000),
 ('DTL003', 'TRX002', 'PSU002', 1, 785000),
-('DTL004', 'TRX002', 'RAM005', 1, 900000);
+('DTL004', 'TRX002', 'RAM005', 1, 900000),
+('DTL005', 'TRX003', 'PSU002', 1, 785000),
+('DTL006', 'TRX003', 'RAM005', 1, 900000),
+('DTL007', 'TRX004', 'RAM002', 1, 1300000),
+('DTL008', 'TRX004', 'FAN009', 1, 702000),
+('DTL009', 'TRX005', 'RAM002', 1, 1300000),
+('DTL010', 'TRX006', 'RAM002', 1, 1300000),
+('DTL011', 'TRX006', 'FAN009', 1, 702000);
 
 -- --------------------------------------------------------
 
@@ -139,8 +180,8 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`id_karyawan`, `nama`, `username`, `password`, `role`) VALUES
-('KRY001', 'Muhamad Damar Hati', 'admin', '$2y$10$24HPranGAzz5RZ3/3K3kjuzUfNBIURQwsKJmvEM37jwS1HU9KMzkS', 'admin'),
-('KRY002', 'Ananta Bagas Sasena', 'admin', '$2y$10$24HPranGAzz5RZ3/3K3kjuzUfNBIURQwsKJmvEM37jwS1HU9KMzkS', 'admin');
+('KRY001', 'Muhamad Damar Hati', 'damar', '$2y$10$24HPranGAzz5RZ3/3K3kjuzUfNBIURQwsKJmvEM37jwS1HU9KMzkS', 'admin'),
+('KRY002', 'Ananta Bagas Sasena', 'remon', '$2y$10$24HPranGAzz5RZ3/3K3kjuzUfNBIURQwsKJmvEM37jwS1HU9KMzkS', 'admin');
 
 -- --------------------------------------------------------
 
@@ -158,15 +199,15 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-('CPU001', 'CPU'),
-('MTB002', 'MOTHERBOARD'),
-('GPU003', 'GPU'),
-('RAM004', 'RAM'),
-('STR005', 'STORAGE'),
-('PSU006', 'PSU'),
 ('CAS007', 'CASE'),
+('CPU001', 'CPU'),
 ('FAN008', 'FAN'),
-('PRP009', 'PERIPHERAL');
+('GPU003', 'GPU'),
+('MTB002', 'MOTHERBOARD'),
+('PRP009', 'PERIPHERAL'),
+('PSU006', 'PSU'),
+('RAM004', 'RAM'),
+('STR005', 'STORAGE');
 
 -- --------------------------------------------------------
 
@@ -210,11 +251,11 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `id_kategori`, `merk`, `spesif
 ('FAN002', 'Aerocool Saturn 12F ARGB 3 Pcs + HUB + Remote', '8', 'AeroCool', '120x120x25mm, 12v CASE, ARGB', 12, 345000),
 ('FAN003', 'Aigo DarkFlash APC1 PWM Controller', '8', 'Aigo', 'PWM CONTROLLER/ARGB REMOTE', 12, 120000),
 ('FAN004', 'Aigo DarkFlash DM12 PRO ARGB 12CM PWM', '8', 'Aigo', '120x120x25mm, 12v CASE, ARGB', 8, 396000),
-('FAN005', 'DeepCool FL12R 3-IN-1 12CM Unique Addressable RGB Fan', '8', 'DeepCool', '120×120×25mm, 12v CASE, ARGB', 6, 800000),
+('FAN005', 'DeepCool FL12R 3-IN-1 12CM Unique Addressable RGB Fan', '8', 'DeepCool', '120×120×25mm, 12v CASE, ARGB', 3, 800000),
 ('FAN006', 'NZXT F120 RGB Core Triple Pack - Matte White', '8', 'NZXT', '120×120×26mm, 12v CASE, ARGB', 8, 995000),
 ('FAN007', 'NZXT F120 RGB Duo Triple Pack - Matte Black', '8', 'NZXT', '120×120×26mm, 12v CASE, ARGB', 7, 1301000),
 ('FAN008', 'Aerocool Mirage L360 360MM ARGB Liquid Cooler', '8', 'AeroCool', '394x120x27mm, 12v CPU, ARGB', 3, 1385000),
-('FAN009', 'Aigo DarkFlash AquaGlow DG240 Black', '8', 'Aigo', '277x120x27mm, 12v CPU, ARGB', 2, 702000),
+('FAN009', 'Aigo DarkFlash AquaGlow DG240 Black', '8', 'Aigo', '277x120x27mm, 12v CPU, ARGB', 0, 702000),
 ('FAN010', 'Aigo DarkFlash DX120 Twister V26 RGB - Black Edition', '8', 'Aigo', ' 75x75x53mm, 12v CPU, ARGB', 5, 526000),
 ('FAN011', 'Aigo DarkFlash Nebula DN360D ARGB - Black Edition', '8', 'Aigo', '397x120x27mm, 12v CPU, ARGB', 1, 1061000),
 ('FAN012', 'Asus ROG Ryujin III 360 ARGB Extreme Liquid Cooler', '8', 'ASUS', ' 3995x120x30 mm, 12v CPU, ARGB', 1, 6040000),
@@ -226,19 +267,19 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `id_kategori`, `merk`, `spesif
 ('GPU005', 'ASRock RX 7600 PHANTOM GAMING OC', '3', 'ASRock', '8GB GDDR6, 128-bit, PCI-e 40', 3, 4930000),
 ('GPU006', 'POWERCOLOR RX 7700XT HELLHOUND', '3', 'POWERCOLOR', '12GB GDDR6, 192-bit, PCI-e 40', 2, 7410000),
 ('GPU007', 'GIGABYTE GEFORCE RTX 5060Ti AERO OC', '3', 'GIGABYTE', '16GB GDDR7, 128-bit, PCI-e 50', 4, 9550000),
-('PRP001', 'Royal Kludge RK R75', '9', 'Royal Kludge', 'K Silver Pro Switch, 80 Tombol + Knob 75%', 2, 648999),
-('PRP002', 'Royal Kludge RK N80', '9', 'Royal Kludge', 'RK Red Switch, 80 + 1 Knob & Smart Screen', 3, 1160000),
-('PRP003', 'AULA F75', '9', 'AULA', 'Reaper Switch + 81 Tombol, Gasket mount', 4, 739000),
-('PRP004', 'AULA WIN60/WIN68', '9', 'AULA', 'Magnetic Switch, Gasket mount', 6, 779000),
-('PRP005', 'Noir Timeless82 V+B552 Classic Edition 75%', '9', 'Noir', '81 Tombol, Gasket mount, LCD Display Screen', 4, 864000),
 ('MBO001', 'MSI B550M PRO-VDH MORTAR WIFI AM4', '2', 'MSI', '4x DDR4, 2x M2, USB32, SATA, WiFi, AM4', 3, 1600000),
 ('MBO002', 'ASRock B550M Steel Legend AM4', '2', 'ASRock', '4x DDR4, 2x M2, USB32, SATA, AM4', 6, 2200000),
 ('MBO003', 'GIGABYTE B450 AORUS Elite V2 AM4', '2', 'GIGABYTE', '4x DDR4, 2x M2, USB32, SATA, AM4', 4, 1650000),
 ('MBO004', 'ASUS B650M-AYW WIFI AM5', '2', 'ASUS', '2x DDR5, 2x M2, USB 32, 4x SATA, WiFi, AM5', 3, 2130000),
 ('MBO005', 'ASROCK B760M PRO RS WIFI WHITE LGA1700', '2', 'ASRock', '4x DDR5, 2x M2, USB 32, 4x SATA, WiFi, LGA1700', 2, 2580000),
 ('MBO006', 'ASROCK PHANTOM GAMING Z790 NOVA WIFI LGA 1700', '2', 'ASRock', '4x DDR5, 4x M2, USB 32, 4x SATA, WiFi, LGA1700', 3, 5630000),
+('PRP001', 'Royal Kludge RK R75', '9', 'Royal Kludge', 'K Silver Pro Switch, 80 Tombol + Knob 75%', 2, 648999),
+('PRP002', 'Royal Kludge RK N80', '9', 'Royal Kludge', 'RK Red Switch, 80 + 1 Knob & Smart Screen', 3, 1160000),
+('PRP003', 'AULA F75', '9', 'AULA', 'Reaper Switch + 81 Tombol, Gasket mount', 4, 739000),
+('PRP004', 'AULA WIN60/WIN68', '9', 'AULA', 'Magnetic Switch, Gasket mount', 6, 779000),
+('PRP005', 'Noir Timeless82 V+B552 Classic Edition 75%', '9', 'Noir', '81 Tombol, Gasket mount, LCD Display Screen', 4, 864000),
 ('PSU001', '1STPLAYER Gaming PSU DK50 500W  80+ Bronze', '6', '1STPLAYER', '80+ Bronze, FULL MODULAR, 500-600W, ATX', 3, 685000),
-('PSU002', '1STPLAYER Gaming PSU DK60 600W 80+ Bronze', '6', '1STPLAYER', '80+ Bronze, FULL MODULAR, 500-600W, ATX', 3, 785000),
+('PSU002', '1STPLAYER Gaming PSU DK60 600W 80+ Bronze', '6', '1STPLAYER', '80+ Bronze, FULL MODULAR, 500-600W, ATX', 2, 785000),
 ('PSU003', 'ADATA XPG PSU Core Reactor II 850w 80+ Gold', '6', 'ADATA', '80+ Gold, FULL MODULAR, 850W, ATX', 5, 1605000),
 ('PSU004', 'ADATA XPG PSU Core Reactor II 750w 80+ Gold', '6', 'ADATA', '80+ Gold, FULL MODULAR, 750W, ATX', 4, 1450000),
 ('PSU005', 'ASRock Challenger CL-750G 750W 80+ Gold', '6', 'ASRock', '80+ Gold, NON MODULAR, 750W, ATX', 5, 1285000),
@@ -251,10 +292,10 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `id_kategori`, `merk`, `spesif
 ('PSU012', 'be quiet! PURE POWER 12 M 750W - 80+ Gold', '6', 'be quiet!', '80+ Gold, FULL MODULAR, 750W, ATX', 8, 1195000),
 ('PSU013', 'be quiet! PURE POWER 12 M 850W - 80+ Gold', '6', 'be quiet!', '80+ Gold, FULL MODULAR, 850W, ATX', 5, 2150000),
 ('RAM001', 'ADATA DDR4 XPG SPECTRIX D35G WHITE 32GB (2X16GB)', '4', 'ADATA', 'DDR4, 3200-3600MHz, RGB', 3, 1570000),
-('RAM002', 'ADATA DDR4 XPG GAMMIX D35 WHITE 16GB (2X8GB)', '4', 'ADATA', 'DDR4, 3200-3600MHz', 3, 1300000),
+('RAM002', 'ADATA DDR4 XPG GAMMIX D35 WHITE 16GB (2X8GB)', '4', 'ADATA', 'DDR4, 3200-3600MHz', 0, 1300000),
 ('RAM003', 'COLORFUL BATTLE AX DDR4 16GB', '4', 'COLORFUL', 'DDR4, 3200MHz', 4, 600000),
 ('RAM004', 'LEXAR DDR4 THOR 16GB (2X8GB)', '4', 'LEXAR', 'DDR4, 3200MHz', 6, 1100000),
-('RAM005', 'ACER PREDATOR VESTA RGB SILVER DDR4 16GB (2X8GB)', '4', 'ACER', 'DDR4, 3200-3600-4000MHz, RGB', 2, 900000),
+('RAM005', 'ACER PREDATOR VESTA RGB SILVER DDR4 16GB (2X8GB)', '4', 'ACER', 'DDR4, 3200-3600-4000MHz, RGB', 10, 900000),
 ('RAM006', 'GSKILL DDR5 TRIDENT Z5 RGB 32GB (2X16GB)', '4', 'GSKILL', 'DDR5, 5600MHz, RGB', 2, 1540000),
 ('RAM007', 'Apacer DDR5 NOX 16GB (2x8GB)', '4', 'Apacer', 'DDR5, 5600MHz', 4, 1200000),
 ('RAM008', 'Crucial Pro OC Black DDR5 32GB (2x16GB)', '4', 'Crucial', 'DDR5, 5600-6000MHz', 2, 2010000),
@@ -277,10 +318,21 @@ INSERT INTO `produk` (`id_produk`, `nama_produk`, `id_kategori`, `merk`, `spesif
 
 CREATE TABLE `supplier` (
   `id_supplier` varchar(20) NOT NULL,
+  `nama` text NOT NULL,
   `alamat` text NOT NULL,
-  `email` text NOT NULL,
-  `telepon` int(20) NOT NULL
+  `telepon` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `supplier`
+--
+
+INSERT INTO `supplier` (`id_supplier`, `nama`, `alamat`, `telepon`) VALUES
+('SUP001', 'PT Sumber Teknologi', 'Jl. Merpati No. 12, Jakarta', '02188991234'),
+('SUP002', 'CV Mandiri Komponen', 'Jl. Cemara Raya No. 55, Bandung', '02276543210'),
+('SUP003', 'PT Digital Nusantara', 'Jl. Diponegoro No. 44, Surabaya', '03199112345'),
+('SUP004', 'UD Maju Bersama', 'Jl. Gajah Mada No. 27, Yogyakarta', '0274556677'),
+('SUP005', 'PT Prima Hardware', 'Jl. Sudirman No. 88, Semarang', '02481726345');
 
 -- --------------------------------------------------------
 
@@ -293,7 +345,7 @@ CREATE TABLE `transaksi` (
   `tanggal` date NOT NULL,
   `total` int(20) NOT NULL,
   `id_customer` varchar(255) DEFAULT NULL,
-  `nama_pembeli` varchar(255) DEFAULT NULL,
+  `nama_pembeli` varchar(20) DEFAULT NULL,
   `id_karyawan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -302,8 +354,12 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id_transaksi`, `tanggal`, `total`, `id_customer`, `nama_pembeli`, `id_karyawan`) VALUES
-('TRX001', '2025-11-16', 1685000, NULL, 'DENI', ''),
-('TRX002', '2025-11-16', 1685000, NULL, 'DENI', '');
+('TRX001', '2025-11-16', 1685000, NULL, NULL, ''),
+('TRX002', '2025-11-16', 1685000, NULL, NULL, ''),
+('TRX003', '2025-11-24', 1685000, NULL, 'DENI', 'KRY001'),
+('TRX004', '2025-11-24', 2002000, 'CUS001', 'Deni Setiawan', 'KRY001'),
+('TRX005', '2025-11-24', 1300000, NULL, 'mansurrr', 'KRY001'),
+('TRX006', '2025-11-24', 2002000, 'CUS001', 'Deni Setiawan', 'KRY001');
 
 --
 -- Indexes for dumped tables
@@ -390,52 +446,7 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT untuk tabel `aktifitas`
 --
 ALTER TABLE `aktifitas`
-  MODIFY `id_aktifitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `aktifitas`
---
-ALTER TABLE `aktifitas`
-  ADD CONSTRAINT `aktifitas_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `barang_keluar`
---
-ALTER TABLE `barang_keluar`
-  ADD CONSTRAINT `barang_keluar_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `barang_keluar_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `barang_masuk`
---
-ALTER TABLE `barang_masuk`
-  ADD CONSTRAINT `barang_masuk_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `barang_masuk_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`),
-  ADD CONSTRAINT `barang_masuk_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`);
-
---
--- Ketidakleluasaan untuk tabel `detail_transaksi`
---
-ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `produk`
---
-ALTER TABLE `produk`
-  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id_aktifitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
